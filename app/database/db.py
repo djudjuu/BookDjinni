8d3472a26b68bf3ad14ccb5e8a6b49decc586f87
djudjuu <julfaber@gmail.com>
2022-08-09 19:28:39 +0200

    docker-compose file

--- app/database/db.py
+++ app/database/db.py
@@ -1,12 +1,14 @@
 from sqlmodel import SQLModel, create_engine
-# import os
-# from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-sqlite_file_name = "database/database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+# when using sqlite
+# sqlite_file_name = "database/database.db"
+# sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-
+# load from .env => not sure whether this is really happening
 # POSTGRES_USER = os.getenv("POSTGRES_USER")
 # POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 # POSTGRES_PORT = os.getenv("POSTGRES_PORT")
@@ -15,15 +17,19 @@ sqlite_url = f"sqlite:///{sqlite_file_name}"
 # POSTGRES_URL = (
 #     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 # )
-postgres_url = "postgresql://djinni:djinni@localhost:5432/djinni"
 
+# for developemnt when api is run locally
+# postgres_url = "postgresql://djinni:djinni@localhost:5432/djinni"
+
+# for developemnt when api is run in container
+POSTGRES_URL = "postgresql://djinni:djinni@djinni_db:5432/djinni"
+
+# TODO look up how to use this
 connect_args = {"check_same_thread": False}
 
+print("POSTGRES_URL", POSTGRES_URL)
 # engine = create_engine(sqlite_url, echo=False, connect_args=connect_args)
-engine = create_engine(postgres_url, echo=True)
-
-# engine = create_engine(sqlite_url, echo=True)
-# engine = create_engine(sqlite_url, echo=False)
+engine = create_engine(POSTGRES_URL, echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
--- readme.md
+++ readme.md
@@ -6,3 +6,27 @@ $ jq -r '.default
  | to_entries[]
 | .key + .value.version' \
  Pipfile.lock > requirements.txt
+
+## a docker container
+
+$ docker-compose up
+
+to start both backend & frontend in a container
+
+TODO: not sure about the environment variables being loaded from a .env file or whether they are being passed in -> for now its hardcoded
+
+# local development
+
+activate virtualenv
+
+$ pipenv shell
+
+$ cd app
+
+$ uvicorn main:app --reload --port
+
+# development inside the container
+
+do a bind mount to have the container be built around the app-directory
+
+-> see docker-compose.yml volume-entry in djinni_backend service -> uncomment ./app:code/app -> not working
